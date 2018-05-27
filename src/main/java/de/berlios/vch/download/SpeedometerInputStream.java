24a0f00d2429a44c2eb827004c8756c9aeaabd88
Henrik Niehaus <henrik.niehaus@gmx.de>
2018-05-27 13:32:48 +0200

    Improved SpeedometerInputStream
    SpeedometerInputStream now maintains a history of speed values,
    calculates an average value over these values and does a bit of
    smoothing to get a more accurate value.

--- src/main/java/de/berlios/vch/download/SpeedometerInputStream.java
+++ src/main/java/de/berlios/vch/download/SpeedometerInputStream.java
@@ -10,7 +10,10 @@ public class SpeedometerInputStream extends FilterInputStream {
     private long lastTime;
     private float speed;
     private int interval = 1;
-    
+    float[] history = new float[10];
+    int historyIndex = 0;
+    int historyCount = 0;
+
     public SpeedometerInputStream(InputStream in) {
         super(in);
     }
@@ -47,7 +50,7 @@ public class SpeedometerInputStream extends FilterInputStream {
         calculateSpeed();
         return skipped;
     }
-    
+
     private void calculateSpeed() {
         long now = System.nanoTime();
         if(lastTime == 0) {
@@ -55,22 +58,51 @@ public class SpeedometerInputStream extends FilterInputStream {
             lastTime = now;
             return;
         }
-        
+
         long diff = now - lastTime;
-        if(diff > (interval * 1000000000l)) {
+        if(diff > (interval * 10000000l)) {
             double bytesPerNanoSec = (double)bytesRead / (double)diff;
             double bytesPerSec = bytesPerNanoSec * 1000000000l;
             float kilobytesPerSec = (float)bytesPerSec / 1024f;
-            speed = kilobytesPerSec;
-            
+            history[historyIndex] = kilobytesPerSec;
+            historyIndex = ++historyIndex % history.length;
+            historyCount = Math.min(++historyCount, history.length);
+
+            if(historyCount > 3) {
+                float sum = 0;
+                for (int i = 0; i < historyCount; i++) {
+                    sum += history[i];
+                }
+                float avg = sum / historyCount;
+
+                float stdDeviation = 0;
+                for (int i = 0; i < historyCount; i++) {
+                    stdDeviation += Math.abs(history[i] - avg);
+                }
+                stdDeviation = stdDeviation / historyCount;
+
+                sum = 0;
+                float avg2 = 0;
+                int counted = 0;
+                for (int i = 0; i < historyCount; i++) {
+                    float value = history[i];
+                    float deviation = Math.abs(avg - value);
+                    if(avg2 == 0 || deviation < stdDeviation) {
+                        sum += value;
+                        avg2 = sum / ++counted;
+                    }
+                }
+                speed = avg2;
+            }
+
             // reset values
             bytesRead = 0;
             lastTime = now;
         }
     }
-    
+
     /**
-     * 
+     *
      * @return the speed in KiB/s for the last {@link #interval} or -1, if no
      *         speed is available currently
      */
